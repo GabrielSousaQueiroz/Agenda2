0aef1d9c11d904398aa9c193d7632d063a2fcc19
Gabriel Sousa <tuygabriel.sousa@gmail.com>
2025-03-15 18:25:29 -0300

    Agenda 4: otimização da Seachbar

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,138 +1,163 @@
 using MauiAppMinhasCompras.Models;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace MauiAppMinhasCompras.Views;
-
-public partial class ListaProduto : ContentPage
+namespace MauiAppMinhasCompras.Views
 {
-    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
-
-    public ListaProduto()
+    public partial class ListaProduto : ContentPage
     {
-        InitializeComponent();
+        ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+        private CancellationTokenSource _debounceTimer;
+        private bool _isRefreshing;
 
-        lst_produtos.ItemsSource = lista;
-    }
-
-    protected async override void OnAppearing()
-    {
-        try
+        public bool IsRefreshing
         {
-            lista.Clear();
-
-            List<Produto> tmp = await App.Db.GetAll();
-
-            tmp.ForEach(i => lista.Add(i));
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Ops", ex.Message, "OK");
-        }
-    }
-
-    private void ToolbarItem_Clicked(object sender, EventArgs e)
-    {
-        try
-        {
-            Navigation.PushAsync(new Views.NovoProduto());
-
-        }
-        catch (Exception ex)
-        {
-            DisplayAlert("Ops", ex.Message, "OK");
-        }
-    }
-
-    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        try
-        {
-            string q = e.NewTextValue;
-
-            lst_produtos.IsRefreshing = true;
-
-            lista.Clear();
-
-            List<Produto> tmp = await App.Db.Search(q);
-
-            tmp.ForEach(i => lista.Add(i));
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Ops", ex.Message, "OK");
-        }
-        finally
-        {
-            lst_produtos.IsRefreshing = false;
-        }
-    }
-
-    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
-    {
-        double soma = lista.Sum(i => i.Total);
-
-        string msg = $"O total é {soma:C}";
-
-        DisplayAlert("Total dos Produtos", msg, "OK");
-    }
-
-    private async void MenuItem_Clicked(object sender, EventArgs e)
-    {
-        try
-        {
-            MenuItem selecinado = sender as MenuItem;
-
-            Produto p = selecinado.BindingContext as Produto;
-
-            bool confirm = await DisplayAlert(
-                "Tem Certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
-
-            if(confirm)
+            get => _isRefreshing;
+            set
             {
-                await App.Db.Delete(p.Id);
-                lista.Remove(p);
+                if (_isRefreshing != value)
+                {
+                    _isRefreshing = value;
+                    OnPropertyChanged();
+                }
             }
         }
-        catch (Exception ex)
+
+        public ListaProduto()
         {
-            await DisplayAlert("Ops", ex.Message, "OK");
+            InitializeComponent();
+            lst_produtos.ItemsSource = lista;
         }
-    }
 
-    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-    {
-        try
+        protected async override void OnAppearing()
         {
-            Produto p = e.SelectedItem as Produto;
+            await CarregarProdutos();
+        }
 
-            Navigation.PushAsync(new Views.EditarProduto
+        private async Task CarregarProdutos()
+        {
+            try
             {
-                BindingContext = p,
-            });
+                lista.Clear();
+                List<Produto> tmp = await App.Db.GetAll();
+                tmp.ForEach(i => lista.Add(i));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
-        catch (Exception ex)
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Ops", ex.Message, "OK");
+            try
+            {
+                Navigation.PushAsync(new Views.NovoProduto());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
-    }
 
-    private async void lst_produtos_Refreshing(object sender, EventArgs e)
-    {
-        try
+        private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
         {
-            lista.Clear();
+            // Cancel previous debounce
+            _debounceTimer?.Cancel();
+            _debounceTimer = new CancellationTokenSource();
 
-            List<Produto> tmp = await App.Db.GetAll();
-
-            tmp.ForEach(i => lista.Add(i));
+            try
+            {
+                // Debounce delay
+                await Task.Delay(500, _debounceTimer.Token);
+                string query = e.NewTextValue;
+                await RealizarBusca(query);
+            }
+            catch (TaskCanceledException)
+            {
+                // Ignorar cancelamentos de tarefas anteriores
+            }
         }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Ops", ex.Message, "OK");
 
-        } finally
+        private async Task RealizarBusca(string query)
         {
-            lst_produtos.IsRefreshing = false;
+            try
+            {
+                IsRefreshing = true;
+                lista.Clear();
+                List<Produto> tmp = await App.Db.Search(query);
+                tmp.ForEach(i => lista.Add(i));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+            finally
+            {
+                IsRefreshing = false;
+            }
+        }
+
+        private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+        {
+            double soma = lista.Sum(i => i.Total);
+            string msg = $"O total é {soma:C}";
+            DisplayAlert("Total dos Produtos", msg, "OK");
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                MenuItem selecionado = sender as MenuItem;
+                Produto p = selecionado.BindingContext as Produto;
+                bool confirm = await DisplayAlert("Tem Certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
+
+                if (confirm)
+                {
+                    await App.Db.Delete(p.Id);
+                    lista.Remove(p);
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            try
+            {
+                Produto p = e.SelectedItem as Produto;
+                Navigation.PushAsync(new Views.EditarProduto
+                {
+                    BindingContext = p,
+                });
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void lst_produtos_Refreshing(object sender, EventArgs e)
+        {
+            try
+            {
+                await CarregarProdutos();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+            finally
+            {
+                lst_produtos.IsRefreshing = false;
+            }
         }
     }
 }
+
